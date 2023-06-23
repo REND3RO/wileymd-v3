00edb46016a1bf15d1c24024f982042c86b2246c
REND3RO <137211374+REND3RO@users.noreply.github.com>
2023-06-23 16:01:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,11 +3,11 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['919172389527', 'TeDevelopers', true]
+  ['13322559715', 'Rend3ro', true]
 ] //owner number 
 
-global.mods = ['916207142994'] 
-global.prems = ['916207142994']
+global.mods = ['13322559715'] 
+global.prems = ['13322559715']
 
 
 
